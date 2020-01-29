a28a98ab846ca559bcf6adc312201e48bd3393df
Gursharan Singh Tatla <gursharan.tatla@gmail.com>
2020-01-29 13:49:16 -0500

    Add files via upload

---
+++ CommissionEmployee.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceExample
+{
+	// Base class that represents a commission employee
+	public class CommissionEmployee
+    {
+		// fields and properties
+
+		private int _id;
+
+		public int Id
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
+
+
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+
+		// gross weekly sales
+		private double _grossSales;
+
+		public double GrossSales
+		{
+			get { return _grossSales; }
+			set
+			{
+				if (value < 0)
+					throw new Exception("Gross sales must be >= 0");
+
+				_grossSales = value;
+			}
+		}
+
+
+		// commission percentage
+		private double _commissionRate;
+
+		public double CommissionRate
+		{
+			get { return _commissionRate; }
+			set
+			{
+				if (value < 0.0 || value > 1.0)
+					throw new Exception("Commission rate must be >=0 or <=1");
+
+				_commissionRate = value;
+			}
+		}
+
+
+		// constructor
+		public CommissionEmployee(int id, string name, double grossSales, double commissionRate)
+		{
+			Id = id;
+			Name = name;
+			GrossSales = grossSales;
+			CommissionRate = commissionRate;
+		}
+
+
+		// calculate the commission employee's pay
+		public double Earnings()
+		{
+			return GrossSales * CommissionRate;
+		}
+
+		
+		// return string representation of CommissionEmployee object
+		public override string ToString()
+		{
+			return $"EmployeeId: {Id}" +
+				$"\nName: {Name}" +
+				$"\nGross Sales: {GrossSales:C}" +
+				$"\nCommission Rate: {CommissionRate:P}";
+		}
+	}
+}
---
+++ SalaryPlusCommissionEmployee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceExample
+{
+	// This is a derived class that inherits from base class CommissionEmployee
+	public class SalaryPlusCommissionEmployee : CommissionEmployee
+    {
+		// fields and properties
+
+		// base salary per week
+		private double _salary;
+
+		public double Salary
+		{
+			get { return _salary; }
+			set { _salary = value; }
+		}
+
+
+		// derived class constructor with call to the base class constructor
+		public SalaryPlusCommissionEmployee(int id, string name, double grossSales, double commissionRate, double salary) : base(id, name, grossSales, commissionRate)
+		{
+			Salary = salary;
+		}
+
+
+		// calculate earnings with call to the base class Earnings() method
+		public new double Earnings()
+		{
+			return Salary + base.Earnings();
+		}
+
+
+		// return string representation of SalaryPlusCommissionEmployee object
+		public override string ToString()
+		{
+			return base.ToString() + $"\nSalary: {Salary:C}";
+		}
+	}
+}
